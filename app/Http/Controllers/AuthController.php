3b4657500841a8ef9944ca036913ecffe9938857
cesar3435ru <cesssuperbro3435@gmail.com>
2023-07-23 00:08:36 -0600

    Se agregaron todas las funciones del crud de usuarios

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    //Crear usuario
+    //Crear usuario pidiendo su rol
     public function formRegister(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -98,7 +98,7 @@ class AuthController extends Controller
 
 
 
-    //Borrar usuario por id
+    //Borrar usuario por id de la BD
     public function deleteUserById($id)
     {
         $user = User::find($id);
@@ -112,6 +112,179 @@ class AuthController extends Controller
         return response()->json(['message' => 'Usuario eliminado correctamente'], 200);
     }
 
+    //Desactivar todos los usuarios sin importar rol
+    public function desactivarAllUsers()
+    {
+        $users = User::all();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 0;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios han sido desactivados correctamente'], 200);
+    }
+
+    //Activar todos los usuarios sin importar rol
+    public function activarAllUsers()
+    {
+        $users = User::all();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 1;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios han sido activados correctamente'], 200);
+    }
+
+
+    // Desactivar administradores (usuarios que tengan role=1)
+    public function desactivarAdmins()
+    {
+        $users = User::where('rol_id', 1)->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario con rol_id igual a 1 en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 0;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios con rol_id igual a 1 han sido desactivados correctamente'], 200);
+    }
+
+    // Desactivar administradores (usuarios que tengan role=2)
+    public function desactivarExperts()
+    {
+        $users = User::where('rol_id', 2)->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario con rol_id igual a 2 en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 0;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios con rol_id igual a 2 han sido desactivados correctamente'], 200);
+    }
+
+    // Desactivar estudiantes (usuarios que tengan role=3)
+    public function desactivarStudents()
+    {
+        $users = User::where('rol_id', 3)->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario con rol_id igual a 3 en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 0;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios con rol_id igual a 3 han sido desactivados correctamente'], 200);
+    }
+
+
+    // Activar administradores (usuarios que tengan role=1)
+    public function activarAdmins()
+    {
+        $users = User::where('rol_id', 1)->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario con rol_id igual a 1 en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 1;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios con rol_id igual a 1 han sido activados correctamente'], 200);
+    }
+
+
+    // Activar expertos (usuarios que tengan role=2)
+    public function activarExperts()
+    {
+        $users = User::where('rol_id', 2)->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario con rol_id igual a 2 en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 1;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios con rol_id igual a 2 han sido activados correctamente'], 200);
+    }
+
+
+    // Activar estudiantes (usuarios que tengan role=3)
+    public function activarStudents()
+    {
+        $users = User::where('rol_id', 3)->get();
+
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun usuario con rol_id igual a 3 en la DB'], 200);
+        }
+
+        foreach ($users as $user) {
+            $user->active = 1;
+            $user->save();
+        }
+
+        return response()->json(['message' => 'Todos los usuarios con rol_id igual a 3 han sido activados correctamente'], 200);
+    }
+
+
+    //Desactivar el usuario por ID
+    public function desactivarUserById($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'Usuario no encontrado'], 404);
+        }
+
+        $user->active = 0;
+        $user->save();
+
+        return response()->json(['message' => 'Usuario desactivado correctamente'], 200);
+    }
+
+    //Activar el usuario por ID
+    public function activarUserById($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'Usuario no encontrado'], 404);
+        }
+
+        $user->active = 1;
+        $user->save();
+
+        return response()->json(['message' => 'Usuario activado correctamente'], 200);
+    }
+
+
+
 
     //Trae todos los usuarios
     public function getAllUsers()
@@ -120,6 +293,54 @@ class AuthController extends Controller
 
         if ($users->isEmpty()) {
             return response()->json(['message' => 'No hay ningun registro en la DB'], 200);
+        }
+
+        return response()->json(['users' => $users], 200);
+    }
+
+
+
+    //Me trae todos los administradores
+    public function getAllAdmins()
+    {
+        $users = User::where('rol_id', 1)
+            ->where(
+                'active',
+                1
+            )->get();
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun registro en la DB'], 200);
+        }
+
+        return response()->json(['users' => $users], 200);
+    }
+
+    //Me trae todos los expertos
+    public function getAllExperts()
+    {
+        $users = User::where('rol_id', 2)
+            ->where(
+                'active',
+                1
+            )->get();
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningun registro en la DB'], 200);
+        }
+
+        return response()->json(['users' => $users], 200);
+    }
+
+
+    //Me trae todos los estudiantes
+    public function getAllStudents()
+    {
+        $users = User::where('rol_id', 3)
+            ->where(
+                'active',
+                1
+            )->get();
+        if ($users->isEmpty()) {
+            return response()->json(['message' => 'No hay ningún registro en la DB'], 200);
         }
 
         return response()->json(['users' => $users], 200);
@@ -151,5 +372,103 @@ class AuthController extends Controller
         $token = $user->createToken('auth-token')->plainTextToken;
 
         return response()->json(['Token' => $token, 'Usuario' => $user, 'AccessToken' => $token], 200);
+    }
+
+
+
+
+    //Crear un administrador y que agregue su rol por defecto
+    public function addAdmin(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'lastname' => 'required|string|max:200',
+            'mat' => 'required|string|max:10',
+            'edad' => 'required',
+            'sexo' => 'required',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'lastname' => $request->lastname,
+            'edad' => $request->edad,
+            'sexo' => $request->sexo,
+            'mat' => $request->mat,
+            'rol_id' => 1,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+
+
+    //Crear un experto y que agregue su rol por defecto
+    public function addExpert(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'lastname' => 'required|string|max:200',
+            'mat' => 'required|string|max:10',
+            'edad' => 'required',
+            'sexo' => 'required',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'lastname' => $request->lastname,
+            'edad' => $request->edad,
+            'sexo' => $request->sexo,
+            'mat' => $request->mat,
+            'rol_id' => 2,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+
+
+    //Crear un estudiante y que agregue su rol por defecto
+    public function addStudent(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'lastname' => 'required|string|max:200',
+            'mat' => 'required|string|max:10',
+            'edad' => 'required',
+            'sexo' => 'required',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'lastname' => $request->lastname,
+            'edad' => $request->edad,
+            'sexo' => $request->sexo,
+            'mat' => $request->mat,
+            'rol_id' => 3,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json(['user' => $user], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //Routes for user
 Route::post('login',[AuthController::class, 'startLogin']); //Iniciar sesion
 Route::post('register',[AuthController::class, 'formRegister']); //Registrar usuario
-Route::put('/actualizar/{id}',[AuthController::class, 'update']); //Actualizar usuarios
+Route::put('/actualizar/{id}',[AuthController::class, 'update']); //Actualizar usuario
 Route::get('/users/{id}',[AuthController::class, 'getUserById']); //Traer usuario por id
-Route::get('users',[AuthController::class, 'getAllUsers']); //Traer todos los usuarios 
-Route::delete('/users/{id}',[AuthController::class, 'deleteUserById']); //Borrar usuario por Id
+Route::get('users',[AuthController::class, 'getAllUsers']); //Traer todos los usuarios sin ninguna condicion
+Route::delete('/users/{id}',[AuthController::class, 'deleteUserById']); //Borrar usuario por Id de la DB
+
+Route::delete('/activaruser/{id}',[AuthController::class, 'activarUserById']); //Activar usuario por Id de la DB, cambiar a 1
+Route::delete('/desactivaruser/{id}',[AuthController::class, 'desactivarUserById']); //Desactivar usuario por Id de la DB, cambiar a 0
+
+Route::delete('activarusers',[AuthController::class, 'activarAllUsers']); //Activar todos los usuarios por Id de la DB sin importar rol, cambiar a 1
+Route::delete('desactivarusers',[AuthController::class, 'desactivarAllUsers']); //Desactivar todos los usuarios por Id de la DB sin importar rol, cambiar a 0
+
+Route::delete('desactivaradmins',[AuthController::class, 'desactivarAdmins']); //Desactivar todos los admins, cambiar active a 0
+Route::delete('desactivarexperts',[AuthController::class, 'desactivarExperts']); //Desactivar todos los expertos, cambiar active a 0
+Route::delete('desactivarstudents',[AuthController::class, 'desactivarStudents']); //Desactivar todos los estudiantes, cambiar active a 0
+
+Route::delete('activaradmins',[AuthController::class, 'activarAdmins']); //Activar todos los admins, cambiar active a 1
+Route::delete('activarexperts',[AuthController::class, 'activarExperts']); //Activar todos los expertos, cambiar active a 1
+Route::delete('activarstudents',[AuthController::class, 'activarStudents']); //Activar todos los estudiantes, cambiar active a 1
+
+
+Route::post('registerAdmin',[AuthController::class, 'addAdmin']); //Registrar admin sin necesidad de poner su rol_id
+Route::post('registerExpert',[AuthController::class, 'addExpert']); //Registrar experto sin necesidad de poner su rol_id
+Route::post('registerStudent',[AuthController::class, 'addStudent']); //Registrar estudiante sin necesidad de poner su rol_id
+Route::get('admins',[AuthController::class, 'getAllAdmins']); //Traer todos los admins que esten activos
+Route::get('experts',[AuthController::class, 'getAllExperts']); //Traer todos los expertos que esten activos
+Route::get('students',[AuthController::class, 'getAllStudents']); //Traer todos los estudiantes que esten activos
+
